64529788642da2dfb1892404bbb2651657c28e6c
wakoIniad <s1f102400606@iniad.org>
2026-03-03 16:07:47 +0900

    save

--- defineCommands.ts
+++ defineCommands.ts
@@ -359,15 +359,20 @@ for(const [name, content] of Object.entries(RootProcessDefine)) {
                 if('subcommands' in entry) {
                     const [i, fs] = getOptionRegister(...entry.subcommands);
                     let T = a => base(a);
-                    for(const [subentry, f] of entry.subcommands.map((E,at)=>[E, fs[at]]) ) {
-                        //const [i, f]: [number, (target: any)=>any] = getOptionRegister(subentry);
+                    //@ts-ignore
+                    fs.reduce((f, F)=> ((target: any) => [addSubcommand, addSubcommandGroup][i](
+                        target,
+                        F
+                    //@ts-ignore
+                    )(f(target))) ,a => base(a));
+
+                    /*for(const f of entry.subcommands.map((E,at)=>[E, fs[at]]) ) {
                         const next = (target: any) => [addSubcommand, addSubcommandGroup][i](
                             target,
                             f
                         );
-                        //res[1].push(next);
                         T = (t=>(a => next(t(a))))(T);
-                    }
+                    }*/
                     res[0] = i;
                     res[1].push(T);
                 }
